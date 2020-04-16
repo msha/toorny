8418d198a528eda06fe7a73aecb6a61bca38fbd0
Lauri <1960498+msha@users.noreply.github.com>
2020-04-16 21:19:07 +0300

    fix bug that allowed user to join tournament multiple times

--- application/users_to_tournaments/models.py
+++ application/users_to_tournaments/models.py
@@ -2,11 +2,15 @@ from application import db
 
 class Users_to_tournaments(db.Model):
     
-    id = db.Column(db.Integer, primary_key=True)
     tournament_id = db.Column(db.Integer, db.ForeignKey('tournament.tournament_id'),
                            nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.users_id'),
                            nullable=False)
+    sort_order = db.Column(db.Integer)
+    
+    __table_args__ = (db.PrimaryKeyConstraint('tournament_id', 'user_id'),
+                     )
+    
 
     def __init__(self, tournament_id, user_id) :
         self.tournament_id = tournament_id
